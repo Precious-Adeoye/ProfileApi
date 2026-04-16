d8685b21ebe021d721dab90f78e47c07d96dd968
Precious-Adeoye <contact.preciousadoye@gmail.com>
2026-04-16 11:49:01 +0100

    add port binding fot program.cs

--- Program.cs
+++ Program.cs
@@ -52,6 +52,9 @@ namespace ProfileApi
                 client.Timeout = TimeSpan.FromSeconds(10);
             });
 
+            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+            builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
